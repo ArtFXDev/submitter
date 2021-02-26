83a2a6f504c6e2c4cd125209420c0dd9b7268617
Soulayrol <sylvain.soulayrol@gmail.com>
2021-02-27 00:11:45 +0100

    update houdini submitter

--- submitter/submitter_engine.py
+++ submitter/submitter_engine.py
@@ -1,5 +1,6 @@
 import os
 import six
+import json
 
 from Qt import QtCore
 from Qt.QtWidgets import QMainWindow
@@ -49,38 +50,30 @@ class SubmitterEngine(Submitter):
             self.custom_layout.addLayout(self.layout_renderer)
 
         if self.engine_name == "houdini":
-            self.output_nodes = []  # {} Key: name, value: Type
-            # Get houdini node
-            cmd = 'C:/Houdini18/bin/hbatch.exe -R -i -q -w -c "opfind -p ./out;quit" {}'.format(self.sid.path)
-            proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            tmp = proc.stdout.read()
-            for line in tmp.split("\n"):
-                line = line.replace(" ", "")
-                if line.startswith("/out/"):
-                    self.output_nodes.append(line)
+            self.output_nodes = {}
+            cache_out_node_path = os.path.join(os.path.dirname(self.sid.path), ".out-nodes.json")
+            if not os.path.exists(cache_out_node_path):
+                cmd = 'C:/Houdini18/bin/hython.exe {} -c "hou.hipFile.save()"'.format(self.sid.path)
+                proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                print("="*30)
+                print("Open the selected scene to get output node connection")
+                tmp = proc.stdout.read()
+                for line in tmp.split("\n"):
+                    print(line)
+                print("... done")
+                print("="*30)
+            if not os.path.exists(cache_out_node_path):
+                self.error("The out node can't be cached in file")
+                raise ValueError("The out node can't be cached in file")
+
+            with open(os.path.join(os.path.dirname(self.sid.path), ".out-nodes.json")) as json_file:
+                self.output_nodes = json.load(json_file)
             self.houdini_layout = QVBoxLayout()
             self.output_node_cb = QComboBox()
-            self.output_node_cb.addItems(self.output_nodes)
+            self.output_node_cb.addItems(self.output_nodes.keys())
             self.houdini_layout.addWidget(self.output_node_cb)
             self.rop_node = None
             self.list_rop = []
-            # chose render
-            self.layout_renderer = QHBoxLayout()
-            self.render_mantra = QRadioButton("Mantra")
-            self.render_arnold = QRadioButton("Arnold")
-            self.render_redshift = QRadioButton("Redshift")
-            self.render_vray = QRadioButton("VRay")
-            if conf.get("renderEngine") == "arnold":
-                self.render_arnold.setChecked(True)
-            if conf.get("renderEngine") == "redshift":
-                self.render_redshift.setChecked(True)
-            if conf.get("renderEngine") == "vray":
-                self.render_vray.setChecked(True)
-            self.layout_renderer.addWidget(self.render_mantra)
-            self.layout_renderer.addWidget(self.render_arnold)
-            self.layout_renderer.addWidget(self.render_redshift)
-            self.layout_renderer.addWidget(self.render_vray)
-            self.houdini_layout.addLayout(self.layout_renderer)
             self.custom_layout.addLayout(self.houdini_layout)
 
     def default_frame_range(self):
@@ -90,17 +83,23 @@ class SubmitterEngine(Submitter):
         path = self.sid.path
         if self.engine_name == "houdini":
             self.rop_node = self.output_node_cb.currentText()
-            if self.render_mantra.isChecked():
-                self.submit(path, self.engine_name)
-            if self.render_arnold.isChecked():
-                self.submit(path, self.engine_name, ["arnold"])
-                conf.set("renderEngine", "arnold")
-            if self.render_redshift.isChecked():
-                self.submit(path, self.engine_name, ["redshift"])
-                conf.set("renderEngine", "redshift")
-            if self.render_vray.isChecked():
-                self.submit(path, self.engine_name, ["vray"])
-                conf.set("renderEngine", "vray")
+
+            def get_render_node(node):
+                if isinstance(self.output_nodes[node], dict):
+                    for key, value in self.output_nodes[node].items():
+                        get_render_node(value)
+                else:
+                    yield (node, self.output_nodes[node])
+            for node, type in get_render_node(self.rop_node):
+                use_renderer = False
+                self.rop_node = node
+                for renderer_loop in ["redshift", "arnold", "vray"]:
+                    if type.startswith(renderer_loop):
+                        self.submit(path, "houdini", [renderer_loop])
+                        use_renderer = True
+                        break
+                if not use_renderer:
+                    self.submit(path, "houdini")
         if self.engine_name == "maya":
             if self.render_default.isChecked():
                 self.submit(path, self.engine_name)
--- submitter/submitter_houdini.py
+++ submitter/submitter_houdini.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 
 import config
+from pipeline.libs.engine.houdini_engine import HoudiniEngine
 from .submitter_base import Submitter
 
 import hou
@@ -19,16 +20,19 @@ class SubmitterHoudini(Submitter):
 
     def __init__(self, parent=get_houdini_window()):
         super(SubmitterHoudini, self).__init__(parent)
-        self.output_node = QComboBox()
-        self.output_node.setEditable(True)
-        self.output_node.addItems(self.get_out_nodes())
-        self.custom_layout.addWidget(self.output_node)
+        self.output_nodes = HoudiniEngine().get_render_nodes()
+        if not self.output_nodes:
+            self.error("Have you render node on your scene ?")
+            raise ValueError("no output node found")
+        self.output_node_cb = QComboBox()
+        self.output_node_cb.setEditable(True)
+        self.output_node_cb.addItems(self.get_out_nodes())
+        self.custom_layout.addWidget(self.output_node_cb)
         self.rop_node = None
         self.list_rop = []
 
     def get_out_nodes(self):
-        out_nodes = [out_node.path() for out_node in hou.node("out/").children()]
-        return out_nodes
+        return self.output_nodes.keys()
 
     def get_path(self):
         return hou.hipFile.path()
@@ -42,37 +46,49 @@ class SubmitterHoudini(Submitter):
     def pre_submit(self):
         path = hou.hipFile.path()
         hou.hipFile.save(file_name=None)
-        # Test output_node
-        if not hou.node(str(self.output_node.currentText())):
+        # Test output_node_cb
+        if not hou.node(str(self.output_node_cb.currentText())):
             self.error("Output node error ! please verify the node path")
         else:
-            # check node type
-            # if merge:
-            #    get number of connected node
-            #    add it to an array
-            # for i in array :
-            #   submit
-            self.rop_node = hou.node(self.output_node.currentText())
-            if self.rop_node.type().name() == "merge":
-                list_rop = self.rop_node.inputs()
-                for rop in list_rop:
-                    self.list_rop.append(rop.path())
-                print("merge detected. Nodes = " + str(self.list_rop))
+            self.rop_node = hou.node(self.output_node_cb.currentText())
+            if self.rop_node not in self.output_nodes.keys():
+                if self.rop_node.type().name() == "merge":
+                    list_rop = self.rop_node.inputs()
+                    for rop in list_rop:
+                        self.list_rop.append(rop.path())
+                    print("merge detected. Nodes = " + str(self.list_rop))
+                else:
+                    self.list_rop = [self.rop_node.path()]
+                for node in self.list_rop:
+                    print("start render for node " + node)
+                    self.rop_node = node
+                    hou.hipFile.save()
+                    _renderer = hou.node(self.rop_node).type().name().lower()
+                    use_renderer = False
+                    for renderer_loop in ["redshift", "arnold", "vray"]:
+                        if _renderer.startswith(renderer_loop):
+                            self.submit(path, "houdini", [renderer_loop])
+                            use_renderer = True
+                            break
+                    if not use_renderer:
+                        self.submit(path, "houdini")
             else:
-                self.list_rop = [self.rop_node.path()]
-            for node in self.list_rop:
-                print("start render for node " + node)
-                self.rop_node = node
-                hou.hipFile.save()
-                _renderer = hou.node(self.rop_node).type().name().lower()
-                use_renderer = False
-                for renderer_loop in ["redshift", "arnold", "vray"]:
-                    if _renderer.startswith(renderer_loop):
-                        self.submit(path, "houdini", [renderer_loop])
-                        use_renderer = True
-                        break
-                if not use_renderer:
-                    self.submit(path, "houdini")
+                def get_render_node(node):
+                    if isinstance(self.output_nodes[node], dict):
+                        for key, value in self.output_nodes[node].items():
+                            get_render_node(value)
+                    else:
+                        yield (node, self.output_nodes[node])
+                for node, type in get_render_node(self.rop_node):
+                    use_renderer = False
+                    self.rop_node = node
+                    for renderer_loop in ["redshift", "arnold", "vray"]:
+                        if type.startswith(renderer_loop):
+                            self.submit(path, "houdini", [renderer_loop])
+                            use_renderer = True
+                            break
+                    if not use_renderer:
+                        self.submit(path, "houdini")
 
     def task_command(self, is_linux, frame_start, frame_end, step, file_path, workspace=""):
         command = [
