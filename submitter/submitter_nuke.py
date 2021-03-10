44360c46c5bda47a380d6ab09d66eb3a3aa4b33b
Soulayrol <sylvain.soulayrol@gmail.com>
2021-03-10 17:39:34 +0100

    Add nuke

--- submitter/submitter_nuke.py
+++ submitter/submitter_nuke.py
@@ -1,8 +1,8 @@
-from Qt.QtWidgets import QRadioButton
+import os
 import nuke
 import config
 from .submitter_base import Submitter
-from Qt.QtWidgets import QApplication
+from Qt.QtWidgets import QApplication, QComboBox, QLabel
 
 app = QApplication.instance()
 
@@ -18,41 +18,41 @@ class SubmitterNuke(Submitter):
 
     def __init__(self, parent=get_nuke_window(), sid=None):
         super(SubmitterNuke, self).__init__(parent, sid)
-        self._rb_render_default = QRadioButton("Default")
-        self._rb_render_redshift = QRadioButton("Redshift")
-        self.custom_layout.addWidget(self._rb_render_default)
-        self.custom_layout.addWidget(self._rb_render_redshift)
+        self.output_node_cb = QComboBox()
+        self.output_node_cb.setMinimumWidth(200)
+        self.output_node_cb.setEditable(True)
+        writeList = []
+        for node in nuke.allNodes('Write'):
+            writeList.append(node.name())
+        self.output_node_cb.addItems(writeList)
+        self.custom_layout.addWidget(QLabel("Output node : "))
+        self.custom_layout.addWidget(self.output_node_cb)
+        self.rop_node = None
 
     def get_path(self):
         return nuke.root()["name"].value()
 
     def default_frame_range(self):
         start = int(nuke.root().firstFrame())
-        end = int(nuke.root().lastFrame()) + 1
+        end = int(nuke.root().lastFrame())
         step = 1
         return (start, end, step)
 
     def pre_submit(self):
         path = nuke.root()["name"].value()
         nuke.scriptSave()
+        self.rop_node = self.output_node_cb.currentText()
         self.submit(path, "nuke")
 
-    def task_command(self, is_linux, frame_start, frame_end, step, file_path, workspace=""):
+    def task_command(self, is_linux, frame_start, frame_end, step, file_path, workspace="", server=None):
         command = [
             config.batcher["nuke"]["render"]["linux" if is_linux else "win"],
-            "-i",
-            "-x",
-            "%D({file_path})".format(file_path=file_path),
+            "-X", self.rop_node,
             "-F", "{start}-{end}x{step}".format(start=str(frame_start), end=str(frame_end), step=str(step)),
+            "-remap", "{source},{target}".format(source=os.getenv("ROOT_PIPE"), target="//{}/PFE_RN_2021".format(server)),
+            "%D({file_path})".format(file_path=file_path),
         ]
         return command
-
-    def set_dirmap(self, local_project, server_project, new_name_path, path):
-        nuke.scriptSaveAs(path)
-        # # # # DIRNAME # # # #
-        nuke.scriptSaveAs(new_name_path)
-        print("Save file : " + str(new_name_path))
-        nuke.scriptOpen(path)
 
 
 def run(sid=None):
